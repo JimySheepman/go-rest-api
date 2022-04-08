af036ccac177d3172c7f7876730d176a6cd862ce
JimySheepman <y.alikoyuncu@gmail.com>
2022-04-09 00:30:13 +0300

    add router

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/JimySheepman/go-rest-api/config/db"
 	"github.com/JimySheepman/go-rest-api/config/env"
 	"github.com/JimySheepman/go-rest-api/internal/router"
 )
@@ -21,16 +20,9 @@ func main() {
 		log.Fatal("Loading .env file failed")
 	}
 
-	database, err := db.ConnectDB()
-	if err != nil {
-		log.Fatal("Cannot connect to database")
-	}
-
-	mux := router.Router(database)
-
 	srv := &http.Server{
-		Handler:      mux,
-		Addr:         "127.0.0.1:8000",
+		Handler:      http.HandlerFunc(router.Serve),
+		Addr:         "0.0.0.0:8000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
