bc304388c691e7dd3396bb646f8ef0930d572e37
JimySheepman <y.alikoyuncu@gmail.com>
2022-04-12 01:45:45 +0300

    graceful shutdown add

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/JimySheepman/go-rest-api/config/env"
@@ -27,5 +31,24 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	log.Fatal(srv.ListenAndServe())
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		fmt.Println("server starting")
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("listenAndServe failed: %v", err)
+		}
+	}()
+	fmt.Println("server started")
+
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("server stopped")
 }
